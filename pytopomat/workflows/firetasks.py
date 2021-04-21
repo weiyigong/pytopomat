089fc24d37ef95a3a865eb89fc8e57cad02ff57e
jeng-yuantsai <tsaie79@gmail.com>
2021-04-21 01:17:49 -0400

    test

--- pytopomat/workflows/firetasks.py
+++ pytopomat/workflows/firetasks.py
@@ -183,7 +183,7 @@ class Vasp2TraceToDb(FiretaskBase):
                 f.write(json.dumps(d, default=DATETIME_HANDLER))
         else:
             db = VaspCalcDb.from_db_file(db_file, admin=True)
-            db.collection = db.db["vasp2trace"]
+            # db.collection = db.db["vasp2trace"]
             db.collection.insert_one(d)
             logger.info("Vasp2trace calculation complete.")
         return FWAction()
